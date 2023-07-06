5a576615dd380bd2f35445cd380a6ee3d451a142
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-06 21:02:01 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
