5df7012a389f73e083d52cea64f93ad840fcab4a
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-06 21:24:59 +0200

    Added custom error class

---
+++ models/http-error.js
@@ -0,0 +1,7 @@
+// Custom error class for handling http errors in a more structured way
+class HttpError extends Error {
+  constructor(message, errorCode) {
+    super(message);
+    this.code = errorCode;
+  }
+}
