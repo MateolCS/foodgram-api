7c8246685be29dedb92b0d64bde9493fca9b1027
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-17 23:10:29 +0200

    Renamed model files to match import statemanets

--- models/post-model.js
+++ models/post-model.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const postSchema = new Schema({
   title: { type: String, required: true },
   image: { type: String, required: true },
-  likes: [{ type: Schema.Types.ObjectId, ref: "Like" }],
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   categories: [{ type: Schema.Types.ObjectId, ref: "Category" }],
