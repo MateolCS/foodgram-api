0195a620a8b7c53d73353c7c4f694808f48e7588
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-18 20:38:07 +0200

    Updated dependencies and added db connection

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
-const app = require("./config/createServer");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const HttpError = require("./models/http-error");
+const connectDB = require("./config/connectDB");
+
+//API routes
+const userRoutes = require("./routes/user-routes");
+const postRoutes = require("./routes/post-routes");
+const commentRoutes = require("./routes/comment-routes");
+const categoryRoutes = require("./routes/category-routes");
+
+const app = express();
+
+app.use(bodyParser.json());
+connectDB();
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/categories", categoryRoutes);
+
+app.use((req, res, next) => {
+  next(new HttpError("Could not find this route.", 404));
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
