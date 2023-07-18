f36993f3b17e713347fcef70dace3880dfea1e66
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-18 19:57:01 +0200

    Implemented controllers for user and category routes

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,10 +3,12 @@ const Category = require("../models/category-model");
 
 const HttpError = require("../models/http-error");
 
+const { validationResult } = require("express-validator");
+
 // @desc    Get all categories
 // @route   GET /api/categories
 // @access  Private
-const getAllCategories = async (req, res) => {
+const getAllCategories = async (req, res, next) => {
   let categories;
   try {
     categories = await Category.find({});
@@ -20,7 +22,7 @@ const getAllCategories = async (req, res) => {
 // @desc    Get category by category id
 // @route   GET /api/categories/:cid
 // @access  Private
-const getCategoryById = async (req, res) => {
+const getCategoryById = async (req, res, next) => {
   const categoryId = req.params.cid;
   let category;
   try {
@@ -34,14 +36,23 @@ const getCategoryById = async (req, res) => {
   }
 
   res.status(200);
-  res.json({ category: category });
+  res.json({ category: category.toObject({ getters: true }) });
 };
 
 // @desc    Create a new category
 // @route   POST /api/categories
 // @access  Private
-const createCategory = async (req, res) => {
+const createCategory = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(
+      new HttpError("Invalid inputs passed, please check your data", 422)
+    );
+  }
+
   const { name } = req.body;
+
   const createdCategory = new Category({
     name,
     posts: [],
@@ -49,7 +60,9 @@ const createCategory = async (req, res) => {
   try {
     await createdCategory.save();
     res.status(201);
-    res.json({ category: createdCategory });
+    res.json({
+      category: createdCategory.toObject({ getters: true }),
+    });
   } catch (error) {
     return next(new HttpError("Could not create category", 500));
   }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,15 +1,98 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const HttpError = require("../models/http-error");
 const User = require("../models/user-model");
 
 // @desc    Register a new user
 // @route   POST /api/users/register
 // @access  Private
-const registerUser = async (req, res, next) => {};
+const registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(new HttpError("Invalid inputs passed", 422));
+  }
+
+  const { username, email, password } = req.body;
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const createdUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+    image: "https://via.placeholder.com/150",
+    posts: [],
+    comments: [],
+  });
+
+  try {
+    await createdUser.save();
+    createdUser.toObject({ getters: true });
+  } catch (error) {
+    return next(new HttpError("Could not create user", 500));
+  }
+
+  const userToReturn = {
+    id: createdUser._id,
+    username: createdUser.username,
+    email: createdUser.email,
+    image: createdUser.image,
+    posts: createdUser.posts,
+    comments: createdUser.comments,
+    token: generateToken(createdUser),
+  };
+
+  res.status(201);
+  res.json({ user: userToReturn });
+};
 
 // @desc   Sign in user
 // @route   POST /api/users/login
 // @access  Private
-const loginUser = async (req, res, next) => {};
+const loginUser = async (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return next(new HttpError("Invalid inputs passed", 422));
+  }
+
+  const { email, password } = req.body;
+
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email })
+      .populate("posts")
+      .populate("comments");
+  } catch (error) {
+    return next(new HttpError("Could not login user", 500));
+  }
+
+  const isMatch = await bcrypt.compare(password, existingUser.password);
+
+  if (!isMatch) {
+    return next(new HttpError("Incorrect password", 401));
+  }
+
+  const userToReturn = {
+    id: existingUser._id,
+    username: existingUser.username,
+    email: existingUser.email,
+    image: existingUser.image,
+    posts: existingUser.posts,
+    comments: existingUser.comments,
+    token: generateToken(existingUser),
+  };
+
+  res.status(200);
+  res.json({ user: userToReturn });
+};
 
 // @desc    Get all users
 // @route   GET /api/users
@@ -17,13 +100,16 @@ const loginUser = async (req, res, next) => {};
 const getAllUsers = async (req, res, next) => {
   let users;
   try {
-    users = await User.find({});
+    users = await User.find({})
+      .select("-password")
+      .populate("posts")
+      .populate("comments");
   } catch (error) {
     return next(new HttpError("Could not get users", 500));
   }
 
   res.status(200);
-  res.json({ users: users });
+  res.json({ users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 // @desc    Get user by user id
@@ -35,13 +121,13 @@ const getUserById = async (req, res, next) => {
   let user;
 
   try {
-    user = await User.findById(userId);
+    user = await User.findById(userId).populate("posts").populate("comments");
   } catch (error) {
     return next(new HttpError("Could not get user, invalid userId", 500));
   }
 
   res.status(200);
-  res.json({ user: user });
+  res.json({ user: user.toObject({ getters: true }) });
 };
 
 // @desc    Update user by user id
@@ -70,7 +156,18 @@ const updateUserById = async (req, res, next) => {
   }
 
   res.status(200);
-  res.json({ user: user });
+  res.json({ user: user.toObject({ getters: true }) });
+};
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      userId: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
 };
 
 module.exports = {
