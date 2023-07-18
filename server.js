4710d7d4e3092c0ecf82ae44ddc328d1548d1d79
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-18 10:51:20 +0200

    Isolated creating server to a config function

--- server.js
+++ server.js
@@ -1,39 +1,4 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-
-const HttpError = require("./models/http-error");
-const connectDB = require("./config/connectDB");
-
-//API routes
-const userRoutes = require("./routes/user-routes");
-const postRoutes = require("./routes/post-routes");
-const commentRoutes = require("./routes/comment-routes");
-const categoryRoutes = require("./routes/category-routes");
-
-const app = express();
-
-app.use(bodyParser.json());
-
-connectDB();
-
-app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/comments", commentRoutes);
-app.use("/api/categories", categoryRoutes);
-
-app.use((req, res, next) => {
-  next(new HttpError("Could not find this route.", 404));
-});
-
-app.use((error, req, res, next) => {
-  if (res.headerSent) {
-    return next(error);
-  }
-
-  res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
-});
+const app = require("./config/createServer");
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
