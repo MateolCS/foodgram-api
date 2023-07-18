1351e595b1cbaa3d4c552ffe4860fb8d3d624eb7
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-18 19:55:52 +0200

    Added category and user routes

--- routes/category-routes.js
+++ routes/category-routes.js
@@ -1,7 +1,17 @@
 const express = require("express");
-
+const { check } = require("express-validator");
 const categoryController = require("../controllers/category-controller");
 
 const router = express.Router();
+
+router.get("/", categoryController.getAllCategories);
+
+router.post(
+  "/",
+  [check("name").not().isEmpty()],
+  categoryController.createCategory
+);
+
+router.get("/:cid", categoryController.getCategoryById);
 
 module.exports = router;
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,7 +1,39 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const userController = require("../controllers/user-controller");
 
 const router = express.Router();
+
+router.get("/", userController.getAllUsers);
+router.get("/:uid", userController.getUserById);
+router.patch("/:uid", userController.updateUserById);
+router.post(
+  "/register",
+  [
+    check("username").not().isEmpty(),
+    check("email").normalizeEmail().isEmail().not().isEmpty(),
+    check("password").isLength({ min: 6 }),
+    check("password-confirmation")
+      .not()
+      .isEmpty()
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          return false;
+        } else {
+          return true;
+        }
+      }),
+  ],
+  userController.registerUser
+);
+router.post(
+  "/login",
+  [
+    check("email").normalizeEmail().isEmail().not().isEmpty(),
+    check("password").isLength({ min: 6 }),
+  ],
+  userController.loginUser
+);
 
 module.exports = router;
