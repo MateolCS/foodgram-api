5bf1756fbaaf2b65ea593b5bb7dc66a0be525370
MateoICS <mateusz.namysl0318@gmail.com>
2023-07-22 23:02:32 +0200

    Started implementing post controller functions

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,25 +1,95 @@
 const mongoose = require("mongoose");
 const Post = require("../models/post-model");
 
+const HttpError = require("../models/http-error");
+
 // @desc    Get all posts
 // @route   GET /api/posts
 // @access  Private
-const getAllPosts = async (req, res, next) => {};
+const getAllPosts = async (req, res, next) => {
+  let posts;
+  try {
+    posts = await Post.find()
+      .populate("creator")
+      .populate("category")
+      .populate("comments");
+    res.status(200);
+    res.json({ posts: posts.map((post) => post.toObject({ getters: true })) });
+  } catch (err) {
+    return next(
+      new HttpError("Fetching posts failed, please try again later", 500)
+    );
+  }
+};
 
 // @desc    Get post by post id
 // @route   GET /api/posts/:pid
 // @access  Private
-const getPostById = async (req, res, next) => {};
+const getPostById = async (req, res, next) => {
+  const postId = req.params.pid;
+
+  let post;
+
+  try {
+    post = await Post.findById(postId)
+      .populate("creator")
+      .populate("comments")
+      .populate("category");
+
+    res.status(200);
+    res.json({ post: post.toObject({ getters: true }) });
+  } catch (err) {
+    return next(
+      new HttpError("Fetching post failed, please try again later", 500)
+    );
+  }
+};
 
 // @desc    Get posts by user id
 // @route   GET /api/posts/:uid
 // @access  Private
-const getPostsByUserId = async (req, res, next) => {};
+const getPostsByUserId = async (req, res, next) => {
+  const userId = req.user.id;
+
+  let posts;
+
+  try {
+    posts = await Post.find({ creator: userId })
+      .populate("creator")
+      .populate("category")
+      .populate("comments");
+
+    res.status(200);
+    res.json({ posts: posts.map((post) => post.toObject({ getters: true })) });
+  } catch (err) {
+    return next(
+      new HttpError("Fetching posts failed, please try again later", 500)
+    );
+  }
+};
 
 // @desc    Get posts by category id
 // @route   GET /api/posts/:cid
 // @access  Private
-const getPostsByCategoryId = async (req, res, next) => {};
+const getPostsByCategoryId = async (req, res, next) => {
+  const categoryId = req.params.cid;
+
+  let posts;
+
+  try {
+    posts = await Post.find({ category: categoryId })
+      .populate("creator")
+      .populate("category")
+      .populate("comments");
+
+    res.status(200);
+    res.json({ posts: posts.map((post) => post.toObject({ getters: true })) });
+  } catch (err) {
+    return next(
+      new HttpError("Fetching posts failed, please try again later", 500)
+    );
+  }
+};
 
 // @desc    Create a new post
 // @route   POST /api/posts
